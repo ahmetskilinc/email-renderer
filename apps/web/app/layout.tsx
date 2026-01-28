3e9c6323e2162348ae999bd99b3ec95bb3c39d39
Ahmet Kilinc <akx9@icloud.com>
2026-01-28 20:20:28 +0000

    adds databuddy

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Suspense } from "react";
 import { Toaster } from "sonner";
+import { Databuddy } from "@databuddy/sdk/react";
 
 export const metadata: Metadata = {
   title: "Email Renderer",
@@ -17,6 +18,8 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased bg-zinc-800">
         <Suspense>{children}</Suspense>
+        <Databuddy
+          clientId="b7fc05bc-70ca-4c88-9a37-085b81593113" />
         <Toaster position="top-right" />
       </body>
     </html>
